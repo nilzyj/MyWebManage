10eee95d7e6d417a87aa4f6a30ea0cd3c5513c04
nilzyj <nilzyj@163.com>
2017-04-23 19:08:35 +0800

    完成修改信息的sql语句

--- Management/src/test/UpdateJsonDataServlet.java
+++ Management/src/test/UpdateJsonDataServlet.java
@@ -16,66 +16,73 @@ import java.sql.Statement;
 
 @WebServlet(name = "UpdateJsonDataServlet", urlPatterns = {"/UpdateJsonDataServlet"})
 public class UpdateJsonDataServlet extends HttpServlet {
-	/**
-	 * 接收报考信息
-	 */
-	private static final long serialVersionUID = 1L;
+    /**
+     * 接收报考信息
+     */
+    private static final long serialVersionUID = 1L;
 
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		doPost(req, resp);
-	}
-	
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		String str = req.getParameter("updateData");
-		str = new String(str.getBytes("ISO8859-1"), "UTF-8");
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        doPost(req, resp);
+    }
 
-		JsonObject jsonObject = null;
-		String name = "";
-		String jsonKey = "";
-		String jsonValue = "";
-		if(!str.isEmpty()) {
-			jsonObject = new JsonParser().parse(str).getAsJsonObject();
-			name = jsonObject.get("name").getAsString();
-			jsonKey = jsonObject.get("infoName").getAsString();
-			jsonValue = jsonObject.get("info").getAsString();
-		}
-		System.out.print(str);
-		System.out.print(name);
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        String str = req.getParameter("updateData");
+        str = new String(str.getBytes("ISO8859-1"), "UTF-8");
 
-		Connection con = null;
-		Statement sm = null;
-		ResultSet rs = null;
-		try {
-			Class.forName("com.mysql.jdbc.Driver").newInstance();
-			String url = "jdbc:mysql://localhost/mydb?useUnicode=true&characterEncoding=utf-8&useSSL=false";
-			con = DriverManager.getConnection(url,"root","root");
-			sm = con.createStatement();
-			rs = sm.executeQuery("SELECT * FROM stu_all_info WHERE stu_name ='"
-					+ name +"'");
-			if(rs.next()) {
-			    sm.executeUpdate("UPDATE stu_all_info SET tags = " +
-						"JSON_REPLACE(stu_info, '$.' + jsonKey, jsonValue) WHERE stu_name = '" + name + "'");
-			}
+        JsonObject jsonObject = null;
+        String name = "";
+        String jsonKey = "";
+        String jsonValue = "";
+        if (!str.isEmpty()) {
+            jsonObject = new JsonParser().parse(str).getAsJsonObject();
+            name = jsonObject.get("name").getAsString();
+            jsonKey = jsonObject.get("infoName").getAsString();
+            jsonValue = jsonObject.get("info").getAsString();
+        }
+        System.out.print(str);
+        System.out.print(name);
 
-			resp.getWriter().write("");
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-		}
-		finally {
-			if(rs != null) {
-				try{rs.close();}catch(Exception e){e.printStackTrace();}
-			}
-			if(sm != null) {
-				try{sm.close();}catch(Exception e){e.printStackTrace();}
-			}
-			if(con != null) {
-				try{con.close();}catch(Exception e){e.printStackTrace();}
-			}
-		}
-	}
+        Connection con = null;
+        Statement sm = null;
+        ResultSet rs = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            String url = "jdbc:mysql://localhost/mydb?useUnicode=true&characterEncoding=utf-8&useSSL=false";
+            con = DriverManager.getConnection(url, "root", "root");
+            sm = con.createStatement();
+            sm.executeUpdate("UPDATE stu_all_info SET stu_info = " +
+                    "JSON_REPLACE(stu_info, '$." + jsonKey + "','" + jsonValue + "'" + ") " +
+                    "WHERE stu_name = '" + name + "'");
+
+            resp.getWriter().write("");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            if (sm != null) {
+                try {
+                    sm.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
